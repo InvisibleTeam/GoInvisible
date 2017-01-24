7c4777c73bd98fdb43d12d4a8b97a66cf04d098a
mbartkowiak <michal.bartkowiak@intive.com>
2017-01-24 14:13:27 +0100

    Fixing static analysis issues

--- app/src/main/java/com/invisibleteam/goinvisible/mvvm/edition/TagsProvider.java
+++ app/src/main/java/com/invisibleteam/goinvisible/mvvm/edition/TagsProvider.java
@@ -3,7 +3,7 @@ package com.invisibleteam.goinvisible.mvvm.edition;
 import com.invisibleteam.goinvisible.model.ImageDetails;
 import com.invisibleteam.goinvisible.model.Tag;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class TagsProvider {
@@ -15,13 +15,12 @@ public class TagsProvider {
     }
 
     public List<Tag> getTags() {
-        return new ArrayList<Tag>() {{
-            add(new Tag("key1", "value1"));
-            add(new Tag("key2", "value2"));
-            add(new Tag("key3", "value3"));
-            add(new Tag("key4", "value4"));
-            add(new Tag("key5", "value5"));
-            add(new Tag("key6", "value6"));
-        }};
+        return Arrays.asList(
+                new Tag("key1", "value1"),
+                new Tag("key2", "value2"),
+                new Tag("key3", "value3"),
+                new Tag("key4", "value4"),
+                new Tag("key5", "value5"),
+                new Tag("key6", "value6"));
     }
 }
