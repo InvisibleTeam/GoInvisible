7bc9b2409176847d5bb332935c9ffc6b56469df0
KamilKucharski <kamil.kucharski@intive.com>
2017-01-27 12:44:36 +0100

    Missing tests method. Unnecessary class exlude.

--- app/src/test/java/com/invisibleteam/goinvisible/util/binding/TextViewBindingTextWatcherTest.java
+++ app/src/test/java/com/invisibleteam/goinvisible/util/binding/TextViewBindingTextWatcherTest.java
@@ -1,12 +1,17 @@
 package com.invisibleteam.goinvisible.util.binding;
 
+import android.text.Editable;
+
 import com.invisibleteam.goinvisible.util.ObservableString;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -26,6 +31,32 @@ public class TextViewBindingTextWatcherTest {
 
         //then
         verify(observableString).set(testString);
+    }
+
+    @Test
+    public void beforeTextChanged_didNothing() {
+        //given
+        TextViewBindingTextWatcher textViewBindingTextWatcher = new TextViewBindingTextWatcher(observableString);
+        String testString = "test";
+
+        //when
+        textViewBindingTextWatcher.beforeTextChanged(testString, 0, 0, 4);
+
+        //then
+        verify(observableString, times(0)).set(testString);
+    }
+
+    @Test
+    public void afterTextChanged_didNothing() {
+        //given
+        TextViewBindingTextWatcher textViewBindingTextWatcher = new TextViewBindingTextWatcher(observableString);
+        Editable mockedEditable = Mockito.mock(Editable.class);
+
+        //when
+        textViewBindingTextWatcher.afterTextChanged(mockedEditable);
+
+        //then
+        verify(observableString, times(0)).set(any());
     }
 
 }
