4af8c91d5f8eafc8025f045c18e5955137347cd5
KamilKucharski <kamil.kucharski@intive.com>
2017-01-30 10:27:55 +0100

    Input types javadocs.

--- app/src/main/java/com/invisibleteam/goinvisible/model/InputType.java
+++ app/src/main/java/com/invisibleteam/goinvisible/model/InputType.java
@@ -1,10 +1,28 @@
 package com.invisibleteam.goinvisible.model;
 
 public enum InputType {
+    /**
+     * Text with no restrictions.
+     */
     TextString,
+    /**
+     * Text containing date.
+     */
     DateString,
+    /**
+     * 0/1 number.
+     */
     BooleanInteger,
+    /**
+     * Integer with no restrictions.
+     */
     ValueInteger,
+    /**
+     * Double with no restrictions.
+     */
     ValueDouble,
+    /**
+     * Latitude, longitude double.
+     */
     PositionDouble
 }
