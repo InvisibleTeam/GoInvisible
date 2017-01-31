bc187ac4ce1b1ba8943ca8b562e78fc6d7f496a1
mbartkowiak <michal.bartkowiak@intive.com>
2017-01-31 10:58:37 +0100

    Permissions request

---
+++ app/src/main/java/com/invisibleteam/goinvisible/mvvm/common/CommonActivity.java
@@ -0,0 +1,77 @@
+package com.invisibleteam.goinvisible.mvvm.common;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.annotation.VisibleForTesting;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+
+import com.invisibleteam.goinvisible.R;
+
+public abstract class CommonActivity extends AppCompatActivity {
+
+    private static final int REQUEST_FOR_PERMISSIONS_REQUEST_CODE = 0;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestForPermissions();
+    }
+
+    @VisibleForTesting(otherwise = VisibleForTesting.PROTECTED)
+    public abstract void prepareView();
+
+    private void requestForPermissions() {
+        final String[] permissions = {
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        if (!arePermissionsGranted(permissions)) {
+            ActivityCompat.requestPermissions(this, permissions, REQUEST_FOR_PERMISSIONS_REQUEST_CODE);
+        } else {
+            prepareView();
+        }
+    }
+
+    private boolean arePermissionsGranted(String[] permissions) {
+        for (String permission : permissions) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_FOR_PERMISSIONS_REQUEST_CODE: {
+                for (int grantResult : grantResults) {
+                    if (grantResult == PackageManager.PERMISSION_DENIED) {
+                        showMissingPermissionsDialog();
+                        return;
+                    }
+                }
+                prepareView();
+                break;
+            }
+            default: {
+                showMissingPermissionsDialog();
+            }
+        }
+    }
+
+    private void showMissingPermissionsDialog() {
+        new AlertDialog
+                .Builder(this, R.style.AlertDialogStyle)
+                .setTitle(R.string.permissions_dialog_title)
+                .setMessage(R.string.permissions_dialog_message)
+                .setPositiveButton(getString(android.R.string.ok).toUpperCase(), (dialog, which) -> requestForPermissions())
+                .setNegativeButton(getString(R.string.exit_app).toUpperCase(), (dialog, which) -> System.exit(0))
+                .setCancelable(false)
+                .show();
+    }
+}
--- app/src/main/java/com/invisibleteam/goinvisible/mvvm/images/ImagesActivity.java
+++ app/src/main/java/com/invisibleteam/goinvisible/mvvm/images/ImagesActivity.java
@@ -2,26 +2,38 @@ package com.invisibleteam.goinvisible.mvvm.images;
 
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
-import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.annotation.VisibleForTesting;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 
 import com.invisibleteam.goinvisible.R;
 import com.invisibleteam.goinvisible.databinding.ActivityImagesBinding;
 import com.invisibleteam.goinvisible.model.ImageDetails;
+import com.invisibleteam.goinvisible.mvvm.common.CommonActivity;
 import com.invisibleteam.goinvisible.mvvm.edition.EditActivity;
 import com.invisibleteam.goinvisible.mvvm.images.adapter.ImagesCompoundRecyclerView;
 
-public class ImagesActivity extends AppCompatActivity implements ImagesView {
+public class ImagesActivity extends CommonActivity implements ImagesView {
 
     private Snackbar snackbar;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void navigateToEdit(ImageDetails imageDetails) {
+        Intent intent = EditActivity.buildIntent(this, imageDetails);
+        startActivity(intent);
+    }
 
+    @Override
+    public void showUnsupportedImageInfo() {
+        snackbar.show();
+    }
+
+    @VisibleForTesting
+    public void setSnackbar(Snackbar snackbar) {
+        this.snackbar = snackbar;
+    }
+
+    @Override
+    public void prepareView() {
         ActivityImagesBinding activityImagesBinding = DataBindingUtil.setContentView(this, R.layout.activity_images);
         setSupportActionBar(activityImagesBinding.mainToolbar);
 
@@ -38,21 +50,5 @@ public class ImagesActivity extends AppCompatActivity implements ImagesView {
                 .make(findViewById(android.R.id.content),
                         R.string.unsupported_extension,
                         Snackbar.LENGTH_LONG);
-    }
-
-    @Override
-    public void navigateToEdit(ImageDetails imageDetails) {
-        Intent intent = EditActivity.buildIntent(this, imageDetails);
-        startActivity(intent);
-    }
-
-    @Override
-    public void showUnsupportedImageInfo() {
-        snackbar.show();
-    }
-
-    @VisibleForTesting
-    public void setSnackbar(Snackbar snackbar) {
-        this.snackbar = snackbar;
     }
 }
