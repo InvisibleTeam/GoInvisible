c57a94a213dc9e71e57c8fb515a1c8e789b32f6a
KamilKucharski <kamil.kucharski@intive.com>
2017-02-08 12:12:58 +0100

    Bounds generating test.

---
+++ app/src/test/java/com/invisibleteam/goinvisible/util/LatLngUtilTest.java
@@ -0,0 +1,28 @@
+package com.invisibleteam.goinvisible.util;
+
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.googlecode.zohhak.api.TestWith;
+import com.googlecode.zohhak.api.runners.ZohhakRunner;
+
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(ZohhakRunner.class)
+public class LatLngUtilTest {
+
+    @TestWith({
+            "17.5,      17.5,       20000,      17.679774668960636,     17.688780123834924",
+            "0,         0,          1000,       0.00899320331800169,    0.008993203428783377"
+    })
+    public void test(double lat, double lng, long radius, double boundsLat, double boundsLng) {
+        //when
+        LatLngBounds bounds = LatLngUtil.generateBoundsWithZoom(new LatLng(lat, lng), radius);
+
+        //then
+        assertEquals(boundsLat, bounds.northeast.latitude, 0.000000001);
+        assertEquals(boundsLng, bounds.northeast.longitude, 0.000000001);
+    }
+
+}
