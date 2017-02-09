3946434553f3d07286b2bf3f8c8180ff03a39a66
KamilKucharski <kamil.kucharski@intive.com>
2017-02-09 16:18:41 +0100

    LeakCanary and StrictMode

---
+++ app/src/main/java/com/invisibleteam/goinvisible/GoInvisibleApplication.java
@@ -0,0 +1,49 @@
+package com.invisibleteam.goinvisible;
+
+
+import android.app.Application;
+import android.os.Build;
+import android.os.StrictMode;
+
+import com.squareup.leakcanary.LeakCanary;
+
+public class GoInvisibleApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        if (BuildConfig.DEBUG) {
+            setupStrictMode();
+            setupLeakCanary();
+        }
+    }
+
+    private void setupStrictMode() {
+        // https://medium.com/@elye.project/walk-through-hell-with-android-strictmode-7e8605168032
+        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                .detectAll()
+                .penaltyLog()
+                .build());
+
+        StrictMode.VmPolicy.Builder vmPolicyBuilder = new StrictMode.VmPolicy.Builder()
+                .detectFileUriExposure()
+                .detectLeakedClosableObjects()
+                .detectLeakedRegistrationObjects()
+                .detectLeakedSqlLiteObjects()
+                .penaltyLog();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            vmPolicyBuilder.detectCleartextNetwork();
+        }
+        StrictMode.setVmPolicy(vmPolicyBuilder.build());
+    }
+
+    private void setupLeakCanary() {
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
+    }
+
+}
