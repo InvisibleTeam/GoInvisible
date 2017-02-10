dfe7bd56d59758b38c8acdd3d17a7e317d97b9b0
KamilKucharski <kamil.kucharski@intive.com>
2017-02-10 13:21:06 +0100

    Test application fix.

--- app/src/main/java/com/invisibleteam/goinvisible/GoInvisibleApplication.java
+++ app/src/main/java/com/invisibleteam/goinvisible/GoInvisibleApplication.java
@@ -4,6 +4,7 @@ package com.invisibleteam.goinvisible;
 import android.app.Application;
 import android.os.Build;
 import android.os.StrictMode;
+import android.support.annotation.VisibleForTesting;
 
 import com.squareup.leakcanary.LeakCanary;
 
@@ -18,7 +19,8 @@ public class GoInvisibleApplication extends Application {
         }
     }
 
-    private void setupStrictMode() {
+    @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
+    protected void setupStrictMode() {
         // https://medium.com/@elye.project/walk-through-hell-with-android-strictmode-7e8605168032
         StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
                 .detectAll()
@@ -37,7 +39,8 @@ public class GoInvisibleApplication extends Application {
         StrictMode.setVmPolicy(vmPolicyBuilder.build());
     }
 
-    private void setupLeakCanary() {
+    @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
+    protected void setupLeakCanary() {
         if (LeakCanary.isInAnalyzerProcess(this)) {
             // This process is dedicated to LeakCanary for heap analysis.
             // You should not init your app in this process.
