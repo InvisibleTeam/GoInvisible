87698ea8dbc041fe18f915f36f5e44ab3c824072
Norbert Leśniewski <nlesniewski@gmail.com>
2017-02-15 16:02:08 +0100

    created list view test class

---
+++ app/src/androidTest/java/com/invisibleteam/goinvisible/tests/ListViewTests.java
@@ -0,0 +1,9 @@
+package com.invisibleteam.goinvisible.tests;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class ListViewTests {
+}
